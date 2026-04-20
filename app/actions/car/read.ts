7946248cf60c072444bb438adcb26d252ba0f603
Mathias Colpaert <mathias.colpaert@trensition.eu>
2026-04-20 18:45:44 +0200

    refactor: remove Car entity scaffolding

--- app/actions/car/read.ts
+++
@@ -1,6 +0,0 @@
-import { Car } from '@/domain/car.model';
-import { dbCarRead } from '@/storage/car/car.read';
-
-export const readCar = async (id: string): Promise<Car> => {
-  return await dbCarRead(id);
-};
--- app/actions/car/search.ts
+++
@@ -1,8 +0,0 @@
-import { Car } from '@/domain/car.model';
-import { CarFilter } from '@/domain/car.filter';
-import { Page } from '@/domain/page.model';
-import { dbCarSearch as searchStorage } from '@/storage/car/car.search';
-
-export const searchCar = async (filter: CarFilter): Promise<Page<Car>> => {
-  return searchStorage(filter);
-};
